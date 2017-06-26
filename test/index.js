86cac054e31b5bf61062c602332ce36b287c0b39
Andrew Harris <andrew@noblesamurai.com>
2017-06-26 19:50:50 +1000

    version that uses back pressure

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const it = require('tape');
 const pollingStream = require('..');
-const { Readable } = require('stream');
+const { Readable, Writable } = require('stream');
 
 it('should be able to poll from a stream', (t) => {
   t.plan(16);
@@ -81,4 +81,36 @@ it('should handle errors', (t) => {
   s.on('end', () => {
     t.fail('stream should not end');
   });
+});
+
+it('should apply backpressure', (t) => {
+  t.plan(1);
+
+  let s = pollingStream(fn, { start: 0 }, { interval: 500, highWaterMark: 1 });
+  function fn(state) {
+    let rs = Readable({
+      objectMode: true,
+      highWaterMark: 1,
+      read: () => rs.push(next())
+    });
+    return rs;
+  }
+
+  // Note: I'm not entirely sure how the back pressure numbers add up...
+  // however setting everything to 1 seems to result in us pushing about 4
+  // items through (I'm guessing the polling streams "pass through" stream
+  // counts as 2, 1 for the "readable" part and 1 for the "writable" part
+
+  let i = 0;
+  function next() {
+    return i < 10 ? i++ : null;
+  }
+
+  let ws = new Writable({ objectMode: true, highWaterMark: 1, write(c) {} });
+  s.pipe(ws);
+
+  setTimeout(() => {
+    t.ok(i < 10);
+    t.end();
+  }, 0);
 });

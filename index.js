8666d897f2252c28c5d1d1d198db80db9b980c57
Andrew Harris <andrew@noblesamurai.com>
2017-06-26 19:50:50 +1000

    version that uses back pressure

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const once = require('once');
-const { Readable, Transform } = require('stream');
-module.exports = function (fn, state = {}, opts) {
-  let finished = false;
+const { Writable, Readable, PassThrough, Transform } = require('stream');
 
-  let interval = opts.interval || 1000;
-  let streamOpts = Object.assign(
-    { objectMode: true, interval: undefined },
-    opts,
-    { read: once(poll) });
+class BatchStream extends PassThrough {
+  constructor (fn, state = {}, opts) {
+    super(Object.assign({}, opts, { objectMode: true }));
+    this.fn = fn;
+    this.state = state;
+    this.finished = false;
+    this.polling = false;
+    this.interval = opts.interval || 1000;
+    this.poll();
+  }
+  poll () {
+    if (this.polling || this.finished) return;
+    this.polling = true;
 
-  const rs = Readable(streamOpts);
-
-  function poll() {
-    const batchStream = fn(state);
+    const batchStream = this.fn(this.state);
     batchStream.once('error', (err) => {
-      rs.emit('error', err);
-      finished = true;
+      this.finished = true;
+      // emit error on the next tick to allow any pending data to be processed first.
+      process.nextTick(this.emit.bind(this), 'error', err);
     });
     batchStream.once('terminate', () => {
-      finished = true;
+      this.finished = true;
+      // end on next tick
+      process.nextTick(this.end.bind(this));
     });
-    batchStream.pipe(Transform({
-      objectMode: true,
-      transform: (chunk, enc, cb) => {
-        rs.push(chunk);
-        cb();
-      },
-      flush: () => {
-        if (finished) {
-          rs.push(null);
-        }
-        else {
-          rs.emit('sync');
-          setTimeout(poll, interval);
-        }
-      }
-    }));
+    batchStream.once('end', () => {
+      batchStream.unpipe(this);
+      if (!this.finished) this.emit('sync');
+      this.polling = false;
+      this.poll();
+      setTimeout(() => this.poll(), this.interval);
+    });
+    batchStream.pipe(this, { end: false });
   }
+}
 
-  return rs;
+module.exports = function (fn, state = {}, opts) {
+  return new BatchStream(fn, state, opts);
 };
